48c988cd153b6adba3b7f5e1e61537f0dbe74f0d
Eduardo Grajales Villanueva <edgravill@gmail.com>
2022-08-29 16:54:06 +0200

    Fix eslint config import

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,37 @@
-const eslintrc = require('./.eslintrc');
-
-module.exports = eslintrc;
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:typescript-sort-keys/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: true,
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-sort-props': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'sort-keys': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
