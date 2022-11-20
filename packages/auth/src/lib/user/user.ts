603216bc2b76d201f98e6a582f667226d1418d4e
Eduardo Grajales Villanueva <edgravill@gmail.com>
2022-11-21 00:17:55 +0100

    Set refs

--- packages/auth/src/lib/user/user.ts
+++ packages/auth/src/lib/user/user.ts
@@ -1,6 +1,6 @@
 import { connectToDB, createIndex } from '../db/dbConnection';
-import { signJWT, verifyJWT } from '../jwt/jwt';
 import { generate2FASecret, verify2FAPasscode } from '@apptoolkit/2fa';
+import { signJWT, verifyJWT } from '@apptoolkit/jwt';
 import { encrypt } from '@apptoolkit/rsa';
 import { compare, hash } from 'bcrypt';
 
